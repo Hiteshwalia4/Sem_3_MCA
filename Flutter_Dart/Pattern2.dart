e514ee4d7b702d67a967fda6ecc061b8e3586c79
Hitesh <hiteshwalia.194071@sggscc.ac.in>
2023-09-05 13:12:08 +0530

    Updated Files

--- Flutter_Dart/Pattern2.dart
+++ Flutter_Dart/Pattern2.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 void main() {
-  stdout.write("Enter the number of rows:");
+  int num =1;
+  stdout.write("Enter the number of rows: ");
   int n = int.parse(stdin.readLineSync() ?? '');
-
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= i; j++) {
-      stdout.write("$j ");
+      stdout.write(num);
+      num+=1;
     }
     stdout.write("\n");
   }
