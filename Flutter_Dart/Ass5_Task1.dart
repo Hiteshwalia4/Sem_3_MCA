f7b70cc17cb71404d66f58f6530d4c24b6e32b69
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2023-10-02 17:11:56 +0530

    Create Ass5_Task1.dart

---
+++ Flutter_Dart/Ass5_Task1.dart
@@ -0,0 +1,58 @@
+// Method 1
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+//This widget is the rest of your application.
+    @override
+    Widget build(BuildContext context) {
+    return MaterialApp(
+    title: 'Flutter Demo',
+    home: Scaffold(
+    appBar: AppBar(
+    title: Text("NRR Application"),
+    ),
+    body: Center(
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text ('My Name is : Hitesh Walia', style: TextStyle(fontSize: 18), ),
+      Text('My Roll No is 003',style: TextStyle(fontSize: 18)),
+      Text( 'My Semester is :3rd Sem', style: TextStyle(fontSize: 18)),
+      Text('My Subject is Computer Science Engineering', style: TextStyle(fontSize: 18)),
+    ],
+),
+),
+),
+);
+}
+}
+
+
+// Method2
+import 'package:flutter/material.dart';
+void main()=>runApp(MaterialApp(
+  title: "Flutter Demo",
+  home: Scaffold(
+   appBar: AppBar(
+     title: Text("NRR Application"),
+   ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text ('My Name is : Hitesh Walia', style: TextStyle(fontSize: 18), ),
+          Text('My Roll No is 003',style: TextStyle(fontSize: 18)),
+          Text( 'My Semester is :3rd Sem', style: TextStyle(fontSize: 18)),
+          Text('My Subject is Computer Science Engineering', style: TextStyle(fontSize: 18)),
+        ],
+      ),
+    ),
+  ),
+)
+);
+
