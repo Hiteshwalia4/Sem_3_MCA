6afa7dfb3c5cfb1113ba7a4ab4c46ad5dc05b01a
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2023-10-19 22:08:08 +0530

    Update Class_Ass1_Task1.dart

--- Flutter_Dart/Class_Assign-1/Class_Ass1_Task1.dart
+++ Flutter_Dart/Class_Assign-1/Class_Ass1_Task1.dart
@@ -1,10 +1,4 @@
-// Method 1
 
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
 
 class MyApp extends StatelessWidget{
 //This widget is the rest of your application.
