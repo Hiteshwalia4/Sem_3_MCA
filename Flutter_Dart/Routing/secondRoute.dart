746b11b1f02c6fdbdaf8abeff8ddea5884031dac
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2023-10-27 23:28:05 +0530

    Create secondRoute.dart

---
+++ Flutter_Dart/Routing/secondRoute.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'firstRoute.dart';
+
+class SecondRoute extends StatelessWidget{
+  final String msg;
+  SecondRoute({super.key,required this.msg});
+  @override
+Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(msg,style:TextStyle(fontSize:20)),
+            // ElevatedButton(onPressed: (){
+            //   Navigator.push(context, MaterialPageRoute(builder: (context)=>MyAppGet()))
+            // }, child: Text("Image Page"),),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);}, child: Text("Go Back"),)
+          ],
+        ),
+      ),
+    );
+  }
+}
