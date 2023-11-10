86a9a835084221095efa1f757e81f5f8525c1caa
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2023-11-10 23:53:38 +0530

    Create ListView_RowWithCardWidget.dart

--- Flutter_Dart/ListView_RowWithCardWidget.dart
+++ Flutter_Dart/ListView_RowWithCardWidget.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class RowWithCardWidget extends StatelessWidget{
+  const RowWithCardWidget({
+    Key? key, required this.index,
+  }) : super(key: key);
+
+  final int index;
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          Icons.flight,
+          size: 58.0,
+          color: Colors.lightBlue,
+        ),
+        title: Text('Student $index'),
+        subtitle: Text('from Best batch'),
+        trailing: Text(
+          ' >  ${index * 70}%',
+          style: TextStyle(color: Colors.lightBlue),
+        ),
+        onTap: () {
+          print('Tapped on Row $index');
+        },
+      ),
+    );
+  }
+}
